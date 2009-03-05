8455b668765ed9609c3b4cf26b8c507b3079b6b1
matt knox <matthewknox@gmail.com>
2009-03-05 12:38:43 -0500

    fixed a FIXME.

--- lib/repl.rb
+++ lib/repl.rb
@@ -1,19 +1,20 @@
-DEFAULT_STARTING_SELF = self
-REPL_CONTEXT_STACK = [binding]
+DEFAULT_STARTING_SELF    = self
+DEFAULT_STARTING_BINDING = binding
+REPL_CONTEXT_STACK = []
 
 module Repl
   def repl
     DEFAULT_STARTING_SELF.cb self
-    REPL_CONTEXT_STACK.push(self)
+    REPL_CONTEXT_STACK.push(binding)
   end
   
   def stop_repl
     REPL_CONTEXT_STACK.pop if (self == REPL_CONTEXT_STACK.last)
-    DEFAULT_STARTING_SELF.cb( REPL_CONTEXT_STACK.empty? ? DEFAULT_STARTING_SELF : REPL_CONTEXT_STACK.pop )
+    DEFAULT_STARTING_SELF.cb((REPL_CONTEXT_STACK.pop or DEFAULT_STARTING_BINDING  ))
   end
 end
 
-module HookRepl
+module HookRepl  # FIXME: not everything unterstands class_eval.  Maybe need an "'eval in this thing' method?"
   def self.replize_object(obj, class_or_instance_or_both = :both)
     obj.class_eval "extend Repl" if [:class, :both].member?(class_or_instance_or_both)
     obj.class_eval "include Repl" if [:object, :both].member?(class_or_instance_or_both)
